76d99f294fdc0197a22cad2b8e21933b9bfdf90f
pd <65471523+prescience-data@users.noreply.github.com>
2026-01-27 16:24:41 +1100

    Create prettier.config.js for formatting rules
    Add Prettier configuration file for code formatting.

---
+++ configs/prettier.config.js
@@ -0,0 +1,26 @@
+export default {
+  /* Core formatting */
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+
+  /* Vue / HTML */
+  vueIndentScriptAndStyle: true,
+  htmlWhitespaceSensitivity: "css",
+
+  /* Line endings */
+  endOfLine: "lf",
+
+  /* Consistency & safety */
+  quoteProps: "as-needed",
+  bracketSameLine: false,
+
+  /* Markdown / prose */
+  proseWrap: "preserve"
+}
